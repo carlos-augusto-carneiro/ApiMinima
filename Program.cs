5a7c7d7999961e5a8d2411afc3f2d1acf49f73b2
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2024-02-08 09:40:58 -0300

    Adicionado os get, post, put, delet

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,5 +22,11 @@ app.UseSwaggerUI(c=>
 });
 
 app.MapGet("/", () => "Vá para o Swagger");
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
